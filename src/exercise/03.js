98f4e004f07ef35e62550c9c4666951ade627774
Dima <idima25@gmail.com>
2023-02-11 13:39:08 -0600

    removed comments

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -30,26 +30,16 @@ function FavoriteAnimal({animal, onAnimalChange}) {
   )
 }
 
-// 🐨 uncomment this
-// function Display({name, animal}) {
-//   return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
-// }
-
-// 💣 remove this component in favor of the new one
 function Display({animal}) {
   return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
-function App() {
-  // 🐨 add a useState for the animal
-  
+function App() {  
   const [animal, setAnimal] = React.useState('')
   return (
     <form>
       <Name />
-      {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
       <FavoriteAnimal animal={animal} onAnimalChange={event => setAnimal(event.target.value)} />
-      {/* 🐨 pass the animal prop here */}
       <Display animal={animal} />
     </form>
   )
